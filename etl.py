8a4bbe657dc66cf898461d717fad75c34914db26
Jim Snavely <ludflu@gmail.com>
2025-02-01 12:32:52 -0500

    improve scope of config variables

--- etl.py
+++ etl.py
@@ -1,29 +1,5 @@
 import polars as pl
 
-initial_filters = {
-    "Species": "Human",
-}
-
-basal_filters = {
-    "Condition": "Basal",
-}
-
-normalization_join = [
-    "population",
-    "reagent",
-    "Donor",
-]
-
-keep_columns = [
-    "population",
-    "reagent",
-    "Condition",
-    "median",
-    "variance",
-]
-
-group_by = ["population", "reagent", "Condition"]
-
 
 def filter_by_group(
     df: pl.DataFrame, by_filter_columns: dict[str, str]
@@ -41,7 +17,7 @@ def filter_by_group_negate(
     return df
 
 
-def filter_data(df: pl.DataFrame) -> pl.DataFrame:
+def filter_data(df: pl.DataFrame, initial_filters: dict[str, str]) -> pl.DataFrame:
     return filter_by_group(df.drop_nans("value"), initial_filters)
 
 
@@ -58,7 +34,9 @@ def remove_outliers(
     )
 
 
-def normalize_by_basal(df: pl.DataFrame) -> pl.DataFrame:
+def normalize_by_basal(
+    df: pl.DataFrame, basal_filters: dict[str, str], normalization_join: list[str]
+) -> pl.DataFrame:
     base = (
         filter_by_group(df, basal_filters)
         .rename({"value": "basal_value"})
@@ -72,7 +50,7 @@ def normalize_by_basal(df: pl.DataFrame) -> pl.DataFrame:
     )
 
 
-def group_by_and_agg(df: pl.DataFrame) -> pl.DataFrame:
+def group_by_and_agg(df: pl.DataFrame, group_by: list[str]) -> pl.DataFrame:
     med = (
         df.group_by(group_by)
         .agg(pl.col("normalized_value").median())
@@ -95,11 +73,35 @@ def save_data(df: pl.DataFrame, file_path: str) -> None:
 
 
 def main():
+    initial_filters = {
+        "Species": "Human",
+    }
+
+    basal_filters = {
+        "Condition": "Basal",
+    }
+
+    normalization_join = [
+        "population",
+        "reagent",
+        "Donor",
+    ]
+
+    keep_columns = [
+        "population",
+        "reagent",
+        "Condition",
+        "median",
+        "variance",
+    ]
+
+    group_by = ["population", "reagent", "Condition"]
+
     df = load_data("sup.csv")
-    df = filter_data(df)
-    df = normalize_by_basal(df)
+    df = filter_data(df, initial_filters)
+    df = normalize_by_basal(df, basal_filters, normalization_join)
     df = remove_outliers(df, group_by, no_std_dev=3)
-    df = group_by_and_agg(df)
+    df = group_by_and_agg(df, group_by)
     df = df.select(keep_columns)
     save_data(df, "data_normalized.csv")
 
